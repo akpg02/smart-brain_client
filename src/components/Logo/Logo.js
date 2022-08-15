d43fefe6a5069c706681817bacb5c81b9d971113
Grace Akpan <opps4lrng@gmail.com>
2022-08-15 13:15:41 -0600

    styling for tilt icon

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,11 +4,13 @@ import "./Logo.css";
 
 const Logo = () => {
   return (
-    <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
-      <div className="Tilt-inner pa3">
-        <img src={brain} alt="logo" style={{ paddingTop: "5px" }} />
-      </div>
-    </Tilt>
+    <div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
+        <div className="Tilt-inner pa3">
+          <img src={brain} alt="logo" style={{ paddingTop: "5px" }} />
+        </div>
+      </Tilt>
+    </div>
   );
 };
 
