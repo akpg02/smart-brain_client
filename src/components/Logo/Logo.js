4c5495b8aa3a99fa37f9cdeba16dbfe12babe44c
Grace Akpan <opps4lrng@gmail.com>
2022-08-15 13:23:41 -0600

    removed un-necessary code

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -15,11 +15,3 @@ const Logo = () => {
 };
 
 export default Logo;
-
-/**
- *  <Tilt className="Tilt br2 shadow-2" perspective={500} scale={1.02}>
-        <div className="Tilt-inner pa3" style={{ height: 150, width: 150 }}>
-          <img src={brain} alt="logo" style={{ paddingTop: "5px" }} />
-        </div>
-      </Tilt>
- */
